36580c9abe852d3facc882bbd023733eaed9d0e5
floodpeak <hongfengzhou@yahoo.com>
2016-10-13 00:38:22 +0800

    refactor, make while into for, shorter

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,9 +1,8 @@
 module.exports = function(number){
   var base = 2
   var result = []
-  while(number % base === 0 && number > 1){
+  for(;number % base === 0 && number > 1;number /= base){
     result.push(base)
-    number /= base
   }
   if(number > 1){
     result.push(number)
