126ddd1591e8c96dd4f033a5eee6d79e323fc347
floodpeak <hongfengzhou@yahoo.com>
2016-10-12 23:36:09 +0800

    refactor, combine the return for 4 and 6

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,8 +1,5 @@
 module.exports = function(number){
-  if(number === 6){
-    return [2,number/2]
-  }
-  if(number === 4){
+  if(number === 6 || number === 4){
     return [2,number/2]
   }
   if(number > 1){
