96f9f607def25439138711a7535bc71db9681d68
floodpeak <hongfengzhou@yahoo.com>
2016-10-12 23:37:18 +0800

    refactor, pull the magic number 2 out

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,6 +1,7 @@
 module.exports = function(number){
+  var base = 2
   if(number === 6 || number === 4){
-    return [2,number/2]
+    return [base,number/base]
   }
   if(number > 1){
     return [number]
