62fc7e397a2551e4284a1c96eaa15cb476d8255d
floodpeak <hongfengzhou@yahoo.com>
2016-10-13 00:52:31 +0800

    refactor, remove unnecessary brace

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,9 +1,7 @@
 module.exports = function(number){
   var result = []
-  for(var base = 2;number > 1;base++){
-    for(;number % base === 0;number /= base){
+  for(var base = 2;number > 1;base++)
+    for(;number % base === 0;number /= base)
       result.push(base)
-    }
-  }
   return result
 }
