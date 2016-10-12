2f44140548f035c111d25eceed78d0d6e5f750a9
floodpeak <hongfengzhou@yahoo.com>
2016-10-12 23:49:33 +0800

    refactor, add a condition for number/base

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,7 +1,11 @@
 module.exports = function(number){
   var base = 2
   if(number % 2 === 0){
-    return [base,number/base]
+    var result = [base]
+    if(number/base !== 1){
+      result.push(number/base)
+    }
+    return result
   }
   if(number > 1){
     return [number]
