7277fb14c5837aeb90aaf5e190cfdc07d9530826
floodpeak <hongfengzhou@yahoo.com>
2016-10-13 00:26:02 +0800

    refactor, change 8 into 4

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -1,12 +1,13 @@
 module.exports = function(number){
-  if(number === 8){
-    return [2,2,2]
-  }
   var base = 2
   var result = []
   if(number % base === 0){
     result.push(base)
     number /= base
+  }
+  if(number === 4){
+    result.push(2)
+    number /= 2
   }
   if(number > 1){
     result.push(number)
