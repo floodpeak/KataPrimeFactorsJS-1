0e7cb6e794309ed1d08cbbf5ea0c72b04a246ac8
floodpeak <hongfengzhou@yahoo.com>
2016-10-13 00:16:02 +0800

    refactor, make the conditions look the same, failed still

--- getPrimeFactors.js
+++ getPrimeFactors.js
@@ -3,8 +3,9 @@ module.exports = function(number){
   var result = []
   if(number % base === 0){
     result.push(base)
-    if(number/base > 1){
-      result.push(number/base)
+    var dividedNumber = number/base
+    if(dividedNumber > 1){
+      result.push(dividedNumber)
     }
   }
   if(number > 1){
